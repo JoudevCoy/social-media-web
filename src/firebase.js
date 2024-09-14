3f67d93b3c0d416262ee74adcdd32ca0d6085f25
JoudevCoy <joudev09@gmail.com>
2024-09-14 12:16:37 +0800

    production tes :)

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  databaseURL: process.env.DBURL,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MSGSENDERID,
-  appId: process.env.APPID,
-  measurementId: process.env.MEASUREMENTID
+  apiKey: "AIzaSyBszfQqPMubYnmBzaGGfgHSgrXhXfUUTMg",
+  authDomain: "joudev-belajar-firebase1-4ec96.firebaseapp.com",
+  databaseURL: "https://joudev-belajar-firebase1-4ec96-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "joudev-belajar-firebase1-4ec96",
+  storageBucket: "joudev-belajar-firebase1-4ec96.appspot.com",
+  messagingSenderId: "91843824903",
+  appId: "1:91843824903:web:8b58df2a7a3b222cc0c9a2",
+  measurementId: "G-42FB2239BV"
 };
 
 const app = initializeApp(firebaseConfig);
